050273ee6c93029318dab6266c0e9613a45fa2bb
Lucas Del Pozzi <lucas@dessin.com.ar>
2017-05-31 15:52:44 -0300

    Continuo

--- src/app/ui/footer.component.ts
+++ src/app/ui/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer',
   template:`
 	    <div class="footer">
             <div class="pull-right">
--- src/app/ui/navigation/navigation.component.ts
+++ src/app/ui/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navigation',
+  selector: 'navigation',
   templateUrl: './navigation.component.html'
 })
 export class NavigationComponent implements OnInit {
