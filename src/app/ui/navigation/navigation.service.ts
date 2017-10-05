10dd25c3cc4b915d646e50ce9361a87926966857
Martin Moreira <mrtnmrr@gmail.com>
2017-10-05 14:14:21 -0300

    Saco ruta vieja del usuario (json server)

--- src/app/ui/navigation/navigation.service.ts
+++ src/app/ui/navigation/navigation.service.ts
@@ -8,9 +8,11 @@ export class NavigationService {
   constructor(private http: Http) { 
 
   }
+  /*
 	getUsuario(): Observable<any>{
 		return this.http.get("http://localhost:3000/profile")
 		.map( (res: Response) => res.json() )
 		.catch((error:any) => Observable.throw(error.json().error || ' Server Error '));
 	}
+	*/
 }
