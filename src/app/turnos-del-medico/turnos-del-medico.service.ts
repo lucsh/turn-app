68d279fba2e53da4b30dc79cdfa48f186f62c1b5
Unknown <juanpabloorlando9498@gmail.com>
2017-11-05 01:04:10 -0300

    <CDLVN-126> #Fixed: ver comentarios

--- src/app/turnos-del-medico/turnos-del-medico.service.ts
+++ src/app/turnos-del-medico/turnos-del-medico.service.ts
@@ -18,236 +18,235 @@ declare var $: any;
 @Injectable()
 export class TurnosDelMedicoService {
 
-    private urlServidor = environment.apiUrl;
+  private urlServidor = environment.apiUrl;
 
-    public turnos$: Observable<Turno[]>;
-    private turnosObserver: Observer<Turno[]>;
+  public turnos$: Observable<Turno[]>;
+  private turnosObserver: Observer<Turno[]>;
 
-    private turnosService: any;
+  private turnosService: any;
 
-    private dataStore: {
-        turnos: Turno[]
-    };
+  private dataStore: {
+    turnos: Turno[]
+  };
 
-    private notificaciones:any;
-
-
-    //private matricula: string;
-    private feathersService;
-    private socket;
-
-    private miMatricula: String;
-    constructor(private FeathersCambiarNombre: Feathers) {
-      //this.socket = io(this.urlServidor);
-      //const feathersApp = feathers().configure(feathers.socketio(this.socket));
-
-      //Obtenemos el service que queremos
-      this.feathersService = FeathersCambiarNombre.devolverFeathers();
-      this.turnosService = this.feathersService.service('turnos');
+  private notificaciones:any;
 
 
-      //Registramos eventos
-      this.turnosService.on('created', (turno) => this.onCreated(turno));
-      this.turnosService.on('updated', (turno) => this.onUpdated(turno));
-      this.turnosService.on('removed', (turno) => this.onRemoved(turno));
-      this.turnosService.on('patched', (turno) => this.onPatched(turno));
+  //private matricula: string;
+  private feathersService;
+  private socket;
+
+  private miMatricula: String;
+  constructor(private FeathersCambiarNombre: Feathers) {
+    //this.socket = io(this.urlServidor);
+    //const feathersApp = feathers().configure(feathers.socketio(this.socket));
+
+    //Obtenemos el service que queremos
+    this.feathersService = FeathersCambiarNombre.devolverFeathers();
+    this.turnosService = this.feathersService.service('turnos');
 
 
-      this.turnos$ = new Observable((observer) => {
-          this.turnosObserver = observer;
-      });
-
-      this.dataStore = { turnos: [] };
-    }
-
+    //Registramos eventos
+    this.turnosService.on('created', (turno) => this.onCreated(turno));
+    this.turnosService.on('updated', (turno) => this.onUpdated(turno));
+    this.turnosService.on('removed', (turno) => this.onRemoved(turno));
+    this.turnosService.on('patched', (turno) => this.onPatched(turno));
 
 
-    public buscarTurnos(miMatricula: String, idMedico) {
-        //let m = this.matricula;
-        // console.log('ENTRE EN BUSCAR TURNOS');
-        this.miMatricula = miMatricula;
+    this.turnos$ = new Observable((observer) => {
+      this.turnosObserver = observer;
+    });
 
-        // console.log(idMedico);
-        let fechaHoy = new Date();
-        // let temp = moment(fechaHoy).subtract(1,'days').format('YYYY-MM-DD');
-        let temp = moment(fechaHoy).format('YYYY-MM-DD');
-        let temp2 = moment(temp, "YYYY-MM-DD").add(1, 'days');
-        let temp3 = (moment(temp2).format('YYYY-MM-DD'));
-
-        // console.log(temp);
-        // console.log(temp2);
-        // console.log(temp3);
-
-        this.turnosService.find({
-            query: {
-                horaInicial: {
-                  $gt: temp,
-                  $lt: temp3
-                },
-                medico: idMedico,
-                $populate: 'paciente medico' //'paciente medico'
-            }
-        }).then((turnos) => {
-
-          // ////console.log("ENTRE AL BUSCAR TURNOS DEL PACIENTES DEL DIA");
-          // ////console.log(turnos);
-
-          ////console.log('ENTRE A PEDIR TURNOS');
-          ////console.log(turnos);
-
-          this.dataStore.turnos = turnos;
-          this.turnosObserver.next(this.dataStore.turnos);
-        }).catch(err => console.error(err));
-    }
-
-
-    public updateTurno(turno, nuevoEstado){
-      var now = new Date();
-      // console.log("################");
-      // console.log(now);
-
-      var nueva = moment(now).utc();
-      // console.log(nueva);
-      // var momentDate = moment(now).utc();
-      // console.log("HORA...");
-      // console.log(momentDate);
-      this.turnosService.patch(turno._id,{"estado": nuevoEstado, "horaUltimoCambio": nueva }).then((turnoActualizado) => {
-        // console.log("Turno actualizado correctamente");
-        // console.log(turnoActualizado);
-      }).catch(err => console.error(err));
-    }
-
-    private getIndex(id: string): number {
-        let foundIndex = -1;
-
-        for (let i = 0; i < this.dataStore.turnos.length; i++) {
-            if (this.dataStore.turnos[i]._id === id) {
-                foundIndex = i;
-            }
-        }
-
-        return foundIndex;
-        // return 0;
-    }
-
-    /*
-        Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onCreated'
-    */
-    private onCreated(turno: any) { //REMPLAZR EL ANY CON TURNO!
-
-        if(this.miMatricula === turno.medico.matricula){
-
-          /*
-            IMPORTANTE:
-            Por el momento, la variable hoy es la correcta, pero la pasamos a local para
-            poder compararla con diaTurno. Es decir, ambos horarios estan en -3 horas.
-          */
-
-          let hoy = new Date();
-
-          hoy.setUTCDate(hoy.getDate());
-          hoy.setUTCHours(hoy.getHours());
-
-
-          let diaTurno = new Date(turno.horaInicial);
-          // diaTurno.setUTCDate(diaTurno.getDate());
-          // diaTurno.setUTCHours(diaTurno.getHours());
-
-          if( diaTurno.getTime() >= hoy.getTime()){
-
-            //No aseguramos que SI O SI pertenezca a hoy
-            if(hoy.getDate() == diaTurno.getDate() && hoy.getMonth() == diaTurno.getMonth()){
-              // console.log('Esto es lo que queriamos!');
-              this.dataStore.turnos.push(turno);
-              //lo pusheo al calendar
-              this.turnosObserver.next(this.dataStore.turnos);
-            }
-          }
-
-        }
-
-    }
-
-    /*
-        Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onUpdated'
-    */
-    private onUpdated(turno: Turno) {
-        const index = this.getIndex(turno._id);
-
-        if(index != -1){
-          this.dataStore.turnos[index] = turno;
-
-          this.turnosObserver.next(this.dataStore.turnos);
-        }
-
-    }
+    this.dataStore = { turnos: [] };
+  }
 
 
 
-    /*
-        Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onRemoved'
-    */
-    private onRemoved(turno: Turno) {
-        const index = this.getIndex(turno._id);
+  public buscarTurnos(miMatricula: String, idMedico) {
+    //let m = this.matricula;
+    // console.log('ENTRE EN BUSCAR TURNOS');
+    this.miMatricula = miMatricula;
 
-        if(index != -1){
-          this.dataStore.turnos.splice(index, 1);
+    // console.log(idMedico);
+    let fechaHoy = new Date();
+    // let temp = moment(fechaHoy).subtract(1,'days').format('YYYY-MM-DD');
+    let temp = moment(fechaHoy).format('YYYY-MM-DD');
+    let temp2 = moment(temp, "YYYY-MM-DD").add(1, 'days');
+    let temp3 = (moment(temp2).format('YYYY-MM-DD'));
 
-          this.turnosObserver.next(this.dataStore.turnos);
-        }
+    // console.log(temp);
+    // console.log(temp2);
+    // console.log(temp3);
 
-    }
-
-    /*
-        Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onUpdated'
-    */
-
-    private onPatched(turno){
-
-      let indexTurno = this.buscarIndexTurno(turno);
-
-      if(indexTurno != -1){
-
-        let turnoAnterior:any = this.dataStore.turnos[indexTurno];
-
-        //El medico esta llamando un nuevo paciente
-        if(turnoAnterior.estado !='en espera' && turno.estado == 'en espera'){
-          // console.log('Estaba pendiente y ahora el paciente llego al consultorio.');
-          this.notificarPacienteEspera(turno.paciente);
-        }
-        this.dataStore.turnos[indexTurno] = turno;
+    this.turnosService.find({
+      query: {
+        horaInicial: {
+          $gt: temp,
+          $lt: temp3
+        },
+        medico: idMedico,
+        $populate: 'paciente medico' //'paciente medico'
       }
+    }).then((turnos) => {
 
+      // ////console.log("ENTRE AL BUSCAR TURNOS DEL PACIENTES DEL DIA");
+      // ////console.log(turnos);
+
+      ////console.log('ENTRE A PEDIR TURNOS');
+      ////console.log(turnos);
+
+      this.dataStore.turnos = turnos;
+      this.turnosObserver.next(this.dataStore.turnos);
+    }).catch(err => console.error(err));
+  }
+
+
+  public updateTurno(turno, nuevoEstado){
+    var now = new Date();
+    // console.log("################");
+    // console.log(now);
+
+    var nueva = moment(now).utc();
+    // console.log(nueva);
+    // var momentDate = moment(now).utc();
+    // console.log("HORA...");
+    // console.log(momentDate);
+    this.turnosService.patch(turno._id,{"estado": nuevoEstado, "horaUltimoCambio": nueva }).then((turnoActualizado) => {
+      // console.log("Turno actualizado correctamente");
+      // console.log(turnoActualizado);
+    }).catch(err => console.error(err));
+  }
+
+  private getIndex(id: string): number {
+    let foundIndex = -1;
+
+    for (let i = 0; i < this.dataStore.turnos.length; i++) {
+      if (this.dataStore.turnos[i]._id === id) {
+        foundIndex = i;
+      }
+    }
+
+    return foundIndex;
+    // return 0;
+  }
+
+  /*
+  Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onCreated'
+  */
+  private onCreated(turno: any) { //REMPLAZR EL ANY CON TURNO!
+
+    if(this.miMatricula === turno.medico.matricula){
+      /*
+      IMPORTANTE:
+      Si hay algun problema, posiblemente haya que sumar +3horas a turnoDate O restar -3horas a diaHoy
+      */
+
+      let hoy = moment(new Date());
+      let momentHoy = hoy.format('YYYY-MM-DD');
+      let diaHoy = (momentHoy.split('-'))[2]; // DD
+
+      let turnoDate = moment(new Date(turno.horaInicial));
+      let momentTurno = turnoDate.format('YYYY-MM-DD');
+      let diaTurno = (momentTurno.split('-'))[2]; // DD
+
+
+      // NO BORRAR: if( turnoDate.valueOf() >= hoy.valueOf()){
+
+      // No aseguramos que SI O SI pertenezca a hoy
+      if(diaTurno == diaHoy && hoy.month() == turnoDate.month()) {
+        // console.log('Esto es lo que queriamos!');
+
+        this.dataStore.turnos.push(turno);
+        // Lo pusheo al componente
+        this.turnosObserver.next(this.dataStore.turnos);
+      }
+      // }
 
 
     }
 
+  }
+
+  /*
+  Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onUpdated'
+  */
+  private onUpdated(turno: Turno) {
+    const index = this.getIndex(turno._id);
+
+    if(index != -1){
+      this.dataStore.turnos[index] = turno;
+
+      this.turnosObserver.next(this.dataStore.turnos);
+    }
+
+  }
 
 
-    /*
-        Al destruirse el servicio, se debe cerrar el socket y borrar el observable del mismo.
-    */
-    ngOnDestroy(){
 
-        //this.socket.close();
-        //this.socket.disconnect();
-        //this.turnosObserver = null;
-        // ////console.log("SE TERMINO EL SERVICIOOOOOOOOOOOOOO");
+  /*
+  Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onRemoved'
+  */
+  private onRemoved(turno: Turno) {
+    const index = this.getIndex(turno._id);
+
+    if(index != -1){
+      this.dataStore.turnos.splice(index, 1);
+
+      this.turnosObserver.next(this.dataStore.turnos);
+    }
+
+  }
+
+  /*
+  Este metodo va a ser llamado cada vez que alguien (desde aca o desde el server) emita ese evento 'onUpdated'
+  */
+
+  private onPatched(turno){
+
+    let indexTurno = this.buscarIndexTurno(turno);
+
+    if(indexTurno != -1){
+
+      let turnoAnterior:any = this.dataStore.turnos[indexTurno];
+
+      //El medico esta llamando un nuevo paciente
+      if(turnoAnterior.estado !='en espera' && turno.estado == 'en espera'){
+        // console.log('Estaba pendiente y ahora el paciente llego al consultorio.');
+        this.notificarPacienteEspera(turno.paciente);
+      }
+      this.dataStore.turnos[indexTurno] = turno;
     }
 
 
-    public asignarNotificaciones(notificaciones){
-      this.notificaciones = notificaciones;
-    }
+
+  }
 
 
-    notificarPacienteEspera(paciente) {
-      this.notificaciones.info(
-        'El paciente ' + paciente.nombre + ' ' + paciente.apellido + ' se encuentra en sala de espera'
-      )
-    }
 
-    //Metodos auxiliares
+  /*
+  Al destruirse el servicio, se debe cerrar el socket y borrar el observable del mismo.
+  */
+  ngOnDestroy(){
+
+    //this.socket.close();
+    //this.socket.disconnect();
+    //this.turnosObserver = null;
+    // ////console.log("SE TERMINO EL SERVICIOOOOOOOOOOOOOO");
+  }
+
+
+  public asignarNotificaciones(notificaciones){
+    this.notificaciones = notificaciones;
+  }
+
+
+  notificarPacienteEspera(paciente) {
+    this.notificaciones.info(
+      'El paciente ' + paciente.nombre + ' ' + paciente.apellido + ' se encuentra en sala de espera'
+    )
+  }
+
+  //Metodos auxiliares
   private buscarIndexTurno(turno): number{
     let indexTurno = -1;
 
