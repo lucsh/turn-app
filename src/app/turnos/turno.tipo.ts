9dfd51e702cd89c7c5c1f88a3343359a889298f0
Juan Pablo <juan.orlando@est.fi.uncoma.edu.ar>
2018-01-25 00:25:52 -0300

    TSLINT done

--- src/app/turnos/turno.tipo.ts
+++ src/app/turnos/turno.tipo.ts
@@ -1,13 +1,13 @@
-export class Turno{
-	//id:string;
-	_id:string;
-	title:string;
-	allDay:boolean;
-	//start:Date;
-	//end:Date;
-	horaInicial:Date;
-	horaFin:Date;
-	paciente:any; //dsps hay que hacer la referencia con el tipo Paciente
-	medico:any; //dsps hay que hacer la referencia con el tipo Paciente
-	color:string; //el color depende del medico, para poder hacer una vista de todos los turnos dados y para que sea facil identificar cada uno de los calendarios.
+export class Turno {
+	// id:string;
+    id: string;
+    title: string;
+    allDay: boolean;
+	// start:Date;
+	// end:Date;
+    horaInicial: Date;
+    horaFin: Date;
+    paciente: any; // dsps hay que hacer la referencia con el tipo Paciente
+    medico: any; // dsps hay que hacer la referencia con el tipo Paciente
+    color: string; // el color depende del medico, para poder hacer una vista de todos los turnos dados y para que sea facil identificar cada uno de los calendarios.
 }
