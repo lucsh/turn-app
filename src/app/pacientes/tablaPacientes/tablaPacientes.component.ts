5a487530f0dcddc2ff6f461e1bac3e6542d81b0b
Juan Pablo <juan.orlando@est.fi.uncoma.edu.ar>
2018-02-23 16:52:45 -0300

    <CDLVN-105> #Done: pacientes.sistema

--- src/app/pacientes/tablaPacientes/tablaPacientes.component.ts
+++ src/app/pacientes/tablaPacientes/tablaPacientes.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 //Para el data table
 import { ElementRef, ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Observable} from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
@@ -12,22 +12,21 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/observable/fromEvent';
 
-import {SelectionModel} from '@angular/cdk/collections';
+import { SelectionModel } from '@angular/cdk/collections';
 
-//Para ordenar la tabla
-import {MdSort} from '@angular/material';
+// Para ordenar la tabla
+import { MdSort } from '@angular/material';
 
-//Para paginar la tabla
-import {MdPaginator} from '@angular/material';
+// Para paginar la tabla
+import { MdPaginator } from '@angular/material';
 
 import { PacientesCompartidosService } from '../../routerService/pacientes.sistema';
 import { Subscription } from 'rxjs/Subscription';
 
-import { PacientesService } from '../pacientes.service';
 import { Paciente } from '../paciente.tipo';
 
 //
-import {default as swal} from 'sweetalert2';
+import { default as swal } from 'sweetalert2';
 declare var $: any;
 
 @Component({
@@ -40,38 +39,38 @@ export class TablaPacientesComponent implements OnInit {
   @Output() pacienteSeleccionado = new EventEmitter();
 
   displayedColumns = ['dni', 'email', 'nombre', 'apellido', 'telefono'];
-  exampleDatabase : ExampleDatabase;
+  exampleDatabase: ExampleDatabase;
   dataSource: ExampleDataSource | null;
 
   selection = new SelectionModel<string>(true, []);
 
   seleccionado = {
-    'id' : '',
-    '_id' : ''
+    'id': '',
+    '_id': ''
   };
   private subscription: Subscription;
-  constructor(private pacientesService: PacientesService, private pacientesCompartidosService: PacientesCompartidosService){
-    this.exampleDatabase = new ExampleDatabase(pacientesService, pacientesCompartidosService);
+  constructor(private pacientesCompartidosService: PacientesCompartidosService) {
+    this.exampleDatabase = new ExampleDatabase(pacientesCompartidosService);
   }
 
   @ViewChild('filter') filter: ElementRef;
   @ViewChild(MdSort) sort: MdSort;
   @ViewChild(MdPaginator) paginator: MdPaginator;
 
-  rowClick(row){
+  rowClick(row) {
     row.seleccionada = !row.seleccionada;
     this.seleccionado = row;
   }
 
-  siguiente(){
+  siguiente() {
     this.pacienteSeleccionado.next(this.seleccionado);
   }
 
   ngOnInit() {
 
     this.seleccionado = {
-      'id' : '',
-      '_id' : ''
+      'id': '',
+      '_id': ''
     };
     // LABEL de items per page de la tabla
     this.paginator._intl.itemsPerPageLabel = 'Pacientes por página';
@@ -79,206 +78,173 @@ export class TablaPacientesComponent implements OnInit {
     this.dataSource = new ExampleDataSource(this.exampleDatabase, this.sort, this.paginator);
 
     Observable.fromEvent(this.filter.nativeElement, 'keyup')
-    .debounceTime(150)
-    .distinctUntilChanged()
-    .subscribe(() => {
-      if (!this.dataSource) {
-        return; }
-        else{
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) {
+          return;
+        }
+        else {
 
           const valorFiltro = this.filter.nativeElement.value;
 
-          if (this.dataSource.filter){
+          if (this.dataSource.filter) {
           }
           this.dataSource.filter = valorFiltro;
         }
       });
-    }
+  }
 
-    onPacienteAgregado(paciente){
-      this.exampleDatabase.addPaciente(paciente);
-    }
+  onPacienteAgregado(paciente) { }
 
-    onPacienteEditado(paciente){
-      this.exampleDatabase.editPaciente(paciente);
-    }
+  onPacienteEditado(paciente) { }
 
 
-    editar(paciente){
+  editar(paciente) {
 
-      this.seleccionado = paciente;
-      /*
-      FIX TEMPORAL: El timeout es para obligar a que el ngIf que proteje el modal,
-      alcance a hacerse true.
-      */
-      setTimeout(() => {
-        $('#formEditarPaciente').modal('show');
-      },
+    this.seleccionado = paciente;
+    /*
+    FIX TEMPORAL: El timeout es para obligar a que el ngIf que proteje el modal,
+    alcance a hacerse true.
+    */
+    setTimeout(() => {
+      $('#formEditarPaciente').modal('show');
+    },
       200);
+  }
+
+
+  onPacienteEliminado(paciente) { }
+
+}
+
+
+// ****************************************************************************
+
+/**
+Base de datos para la tabla.
+*/
+export class ExampleDatabase {
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<Paciente[]> = new BehaviorSubject<Paciente[]>([]);
+  get data(): Paciente[] { return this.dataChange.value; }
+  private subscription: Subscription;
+
+  constructor(private pacientesCompartidosService: PacientesCompartidosService) {
+    this.observarPacientes();
+  }
+
+  observarPacientes() {
+    /*
+      Subscribimos a los pacientes, para que tengan una correspondencia
+      con los pacientes del navigator
+    */
+    if (this.pacientesCompartidosService.pacientes$) {
+      this.subscription = this.pacientesCompartidosService.pacientes$.subscribe((pacientes) => {
+        console.log(pacientes);
+        this.setPacientes(pacientes);
+        // this.ref.markForCheck();
+      }, (err) => {
+        console.log('Error en observarPacientes de tablaPacientes');
+        console.error(err);
+      });
+
+      // Obtenemos los pacientes compartidos
+      this.pacientesCompartidosService.getPacientes();
     }
-
-
-  onPacienteEliminado(paciente){
-    this.exampleDatabase.removePaciente(paciente);
   }
 
-  }
-
-
-  //****************************************************************************
 
   /**
-  Base de datos para la tabla.
+  Pasamos nuestros trabajos al observer
   */
-  export class ExampleDatabase {
-    /** Stream that emits whenever the data has been modified. */
-    dataChange: BehaviorSubject<Paciente[]> = new BehaviorSubject<Paciente[]>([]);
-    get data(): Paciente[] { return this.dataChange.value; }
-    private subscription: Subscription;
-
-    constructor(private pacientesService: PacientesService, private pacientesCompartidosService: PacientesCompartidosService) {
-      this.observarPacientes();
-    }
-
-    observarPacientes(){
-      /*
-        Subscribimos a los pacientes, para que tengan una correspondencia
-        con los pacientes del navigator
-      */
-      if (this.pacientesCompartidosService.pacientes$){
-        this.subscription = this.pacientesCompartidosService.pacientes$.subscribe((pacientes) => {
-
-          this.setPacientes(pacientes);
-          // this.ref.markForCheck();
-        }, (err) => {
-          console.log('Error en observarPacientes de tablaPacientes');
-          console.error(err);
-        });
-
-        // Obtenemos los pacientes compartidos
-        this.pacientesCompartidosService.getPacientes();
-      }
-    }
-
-
-    /**
-    Pasamos nuestros trabajos al observer
-    */
-    setPacientes(pacientes: Paciente[]) {
-      const copiedData = pacientes;
-      this.dataChange.next(pacientes);
-
-    }
-
-
-    addPaciente(paciente) {
-      const copiedData = this.data.slice();
-
-      this.pacientesCompartidosService.addPaciente(paciente);
-    }
-
-    editPaciente(pacienteEditado){
-        this.pacientesCompartidosService.updatePaciente(pacienteEditado);
-    }
-
-    removePaciente(paciente){
-      const copiedData = this.data.slice();
-      console.log('El paciente a remover es: ');
-      console.log(paciente);
-
-      let indice = -1;
-      copiedData.forEach(function(elem, index){
-        if (elem._id == paciente._id){
-          indice = index;
-        }
-      });
-      if (indice > -1) {
-        copiedData.splice(indice, 1);
-        this.dataChange.next(copiedData);
-      }
-    }
+  setPacientes(pacientes: Paciente[]) {
+    const copiedData = pacientes;
+    this.dataChange.next(pacientes);
 
   }
 
+}
 
-  //****************************************************************************
+
+//****************************************************************************
+
+/**
+Esta clase solo se encarga de hacer el renderizado de la tabla,
+basandose en los datos de ExampleDatabase.
+*/
+export class ExampleDataSource extends DataSource<any> {
+  _filterChange = new BehaviorSubject('');
+  get filter(): string { return this._filterChange.value; }
+  set filter(filter: string) { this._filterChange.next(filter); }
+
+
+  filteredData: Paciente[] = [];
+  renderedData: Paciente[] = [];
+
+  constructor(private _exampleDatabase: ExampleDatabase, private _sort: MdSort, private _paginator: MdPaginator) {
+    super();
+  }
 
   /**
-  Esta clase solo se encarga de hacer el renderizado de la tabla,
-  basandose en los datos de ExampleDatabase.
+  Esta funcion es llamada por la tabla para buscar el stream de datos para renderizar.
   */
-  export class ExampleDataSource extends DataSource<any> {
-    _filterChange = new BehaviorSubject('');
-    get filter(): string { return this._filterChange.value; }
-    set filter(filter: string) { this._filterChange.next(filter); }
+  connect(): Observable<Paciente[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._filterChange,
+      this._sort.mdSortChange,
+      this._paginator.page
+    ];
 
+    return Observable.merge(...displayDataChanges).map(() => {
+      // console.log(displayDataChanges);
 
-    filteredData: Paciente[] = [];
-    renderedData: Paciente[] = [];
+      //Preparamos el FILTRO de la tabla
+      this.filteredData = this._exampleDatabase.data.slice().filter((item: Paciente) => {
 
-    constructor(private _exampleDatabase: ExampleDatabase,  private _sort: MdSort,  private _paginator: MdPaginator) {
-      super();
-    }
+        // Concatenamos los filtros para armar el string de busqueda
+        const searchStr = (item.dni + item.email + item.nombre + item.apellido + item.telefono).toLowerCase();
 
-    /**
-    Esta funcion es llamada por la tabla para buscar el stream de datos para renderizar.
-    */
-    connect(): Observable<Paciente[]> {
-      const displayDataChanges = [
-        this._exampleDatabase.dataChange,
-        this._filterChange,
-        this._sort.mdSortChange,
-        this._paginator.page
-      ];
-
-      return Observable.merge(...displayDataChanges).map(() => {
-        // console.log(displayDataChanges);
-
-        //Preparamos el FILTRO de la tabla
-        this.filteredData =   this._exampleDatabase.data.slice().filter((item: Paciente) => {
-
-          // Concatenamos los filtros para armar el string de busqueda
-          const searchStr = (item.dni + item.email + item.nombre + item.apellido + item.telefono).toLowerCase();
-
-          return searchStr.indexOf(this.filter.toLowerCase()) != -1;
-        });
-
-        // Ordenamiento de datos
-        const sortedData = this.sortData(this.filteredData.slice());
-
-        // Grab the page's slice of the filtered sorted data.
-        const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-        this.renderedData = sortedData.splice(startIndex, this._paginator.pageSize);
-
-
-        return this.renderedData;
+        return searchStr.indexOf(this.filter.toLowerCase()) != -1;
       });
-    }
 
-    disconnect() {}
+      // Ordenamiento de datos
+      const sortedData = this.sortData(this.filteredData.slice());
 
-    /**
-    Retorna una copia ordenada de los datos.
-    */
-    sortData(data: Paciente[]): Paciente[] {
-      if (!this._sort.active || this._sort.direction == '') { return data; }
+      // Grab the page's slice of the filtered sorted data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      this.renderedData = sortedData.splice(startIndex, this._paginator.pageSize);
 
-      return data.sort((a, b) => {
-        let propertyA: number|string = '';
-        let propertyB: number|string = '';
 
-        switch (this._sort.active) {
-          case 'dni': [propertyA, propertyB] = [a.dni, b.dni]; break;
-          case 'email': [propertyA, propertyB] = [a.email, b.email]; break;
-          case 'nombre': [propertyA, propertyB] = [a.nombre, b.nombre]; break;
-          case 'apellido': [propertyA, propertyB] = [a.apellido, b.apellido]; break;
-          case 'telefono': [propertyA, propertyB] = [a.telefono, b.telefono]; break;
-        }
-
-        const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-        const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-
-        return (valueA < valueB ? -1 : 1) * (this._sort.direction == 'asc' ? 1 : -1);
-      });
-    }
+      return this.renderedData;
+    });
   }
+
+  disconnect() { }
+
+  /**
+  Retorna una copia ordenada de los datos.
+  */
+  sortData(data: Paciente[]): Paciente[] {
+    if (!this._sort.active || this._sort.direction == '') { return data; }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'dni': [propertyA, propertyB] = [a.dni, b.dni]; break;
+        case 'email': [propertyA, propertyB] = [a.email, b.email]; break;
+        case 'nombre': [propertyA, propertyB] = [a.nombre, b.nombre]; break;
+        case 'apellido': [propertyA, propertyB] = [a.apellido, b.apellido]; break;
+        case 'telefono': [propertyA, propertyB] = [a.telefono, b.telefono]; break;
+      }
+
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction == 'asc' ? 1 : -1);
+    });
+  }
+}
--- src/app/routerService/pacientes.sistema.ts
+++ src/app/routerService/pacientes.sistema.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers} from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import { Paciente } from '../pacientes/paciente.tipo';
@@ -9,13 +9,15 @@ import { AuthService } from '../authentication/auth.service';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-
+import { environment } from '../../environments/environment';
 
 import { Observer } from 'rxjs/Observer';
 import { Subscription } from 'rxjs/Subscription';
-import { Subject }           from 'rxjs/Subject';
-
+import { Subject } from 'rxjs/Subject';
 import * as Rx from 'rxjs';
+
+// Librerias de feathers
+import { Feathers } from '../authentication/feathers.service';
 @Injectable()
 export class PacientesCompartidosService {
 
@@ -23,80 +25,116 @@ export class PacientesCompartidosService {
   public observer: Observer<any[]>;
   pacientes$: Observable<any[]>;
 
+  // Refresh por socket
+  private feathersService;
+  private pacientesSocketService;
+
   constructor(
-    private pacientesService: PacientesService
-  ){
+    private pacientesService: PacientesService,
+    private feathersAuthenticated: Feathers
+  ) {
+    // Conectamos con servidor Featherjs
+    this.subscribeToServer();
 
     this.pacientes$ = new Observable((observer) => {
       this.observer = observer;
     });
   }
-  public iniciarPacientes(pacientes){
+  public iniciarPacientes(pacientes) {
     this.pacientes = pacientes;
     this.getPacientes();
   }
 
-  public addPaciente(paciente){
-    if (paciente){
+  public addPaciente(paciente) {
+
+    const existePaciente = this.existePaciente(paciente);
+
+    // Solo agregamos si no existia el paciente
+    if (!existePaciente) {
       this.pacientes.push(paciente);
       this.observer.next(this.pacientes);
     }
   }
 
-  public getPacientes(){
-      this.observer.next(this.pacientes);
+  public getPacientes() {
+    this.observer.next(this.pacientes);
   }
 
   public updatePaciente(paciente) {
-    if (this.pacientes.length > 0 && paciente){
-      let encontrado = -1;
-      this.pacientes.forEach(function(elem, index){
-        if (elem._id == paciente._id){
-          encontrado = index;
-        }
-      });
-      if (encontrado > -1){
-        this.pacientes[encontrado] = paciente;
-      }
+
+    const encontrado = this.searchPaciente(paciente);
+
+    if (encontrado > -1) {
+      this.pacientes[encontrado] = paciente;
+      this.observer.next(this.pacientes);
+    }
+
+  }
+
+  public deletePaciente(paciente) {
+
+    const encontrado = this.searchPaciente(paciente);
+    if (encontrado > -1) {
+      this.pacientes.splice(encontrado, 1);
       this.observer.next(this.pacientes);
     }
   }
 
-  public deletePaciente(paciente){
-    if (this.pacientes.length > 0 && paciente){
-      let encontrado = -1;
-      this.pacientes.forEach(function(elem, index){
-        if (elem._id == paciente._id){
-          encontrado = index;
-        }
-      });
-      if (encontrado > -1){
-        this.pacientes.splice(encontrado, 1);
-      }
-      this.observer.next(this.pacientes);
-    }
-  }
+  public existePaciente(paciente) {
 
-  public existePaciente(paciente){
+    const indice = this.searchPaciente(paciente);
 
-    const indice = this.buscarPaciente(paciente);
-
-    if (indice > -1){
+    if (indice > -1) {
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
 
-  private buscarPaciente(paciente){
-    let indice = -1;
-    this.pacientes.forEach(function(elem, index){
-      if (elem._id == paciente._id){
-        indice = index;
-      }
-    });
+  public subscribeToServer() {
 
+    // Estamos usando el Service de Feathers, pues el que tiene la autenticacion del login
+    this.feathersService = this.feathersAuthenticated.devolverFeathers();
+    // Obtenemos el service que queremos
+    this.pacientesSocketService = this.feathersService.service('pacientes');
+
+    // Registramos eventos
+    this.pacientesSocketService.on('created', (paciente) => this.onCreated(paciente));
+    this.pacientesSocketService.on('updated', (paciente) => this.onUpdated(paciente));
+    this.pacientesSocketService.on('removed', (paciente) => this.onRemoved(paciente));
+    this.pacientesSocketService.on('patched', (paciente) => this.onPatched(paciente));
+  }
+
+  private onCreated(paciente) {
+    this.addPaciente(paciente);
+  }
+
+  private onUpdated(paciente) {
+    this.updatePaciente(paciente);
+  }
+
+  private onRemoved(paciente) {
+    this.deletePaciente(paciente);
+  }
+
+  private onPatched(paciente) {
+    if (paciente.eliminado) {
+      this.deletePaciente(paciente);
+    } else {
+      this.updatePaciente(paciente);
+    }
+  }
+
+  private searchPaciente(paciente) {
+    let indice = -1;
+    if (paciente && this.pacientes.length > 0) {
+      this.pacientes.forEach(function (elem, index) {
+        if (elem._id == paciente._id) {
+          indice = index;
+        }
+      });
+    }
     return indice;
 
   }
