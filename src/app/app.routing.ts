8c6b235c0e1c18a6ce627d8f25d5994d4568ba26
JuampiOrlando <juanpabloorlando9498@gmail.com>
2018-04-11 18:36:24 -0300

    linter sobre app.routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,67 +1,67 @@
 
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {TurnosComponent} from './turnos/turnos.component';
-import {PacientesComponent} from './pacientes/pacientes.component';
-import {ObrasComponent} from './obras/obras-main/obras.component';
-import {TurnosDelMedicoComponent} from './turnos-del-medico/turnos-del-medico.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TurnosComponent } from './turnos/turnos.component';
+import { PacientesComponent } from './pacientes/pacientes.component';
+import { ObrasComponent } from './obras/obras-main/obras.component';
+import { TurnosDelMedicoComponent } from './turnos-del-medico/turnos-del-medico.component';
 
 import { AuthGuard } from './authentication/auth.guard';
 import { AdministrativoGuard } from './authentication/administrativo.guard';
 import { MedicoGuard } from './authentication/medico.guard';
 
 
-import { LoginComponent }     from './authentication/login.component';
+import { LoginComponent } from './authentication/login.component';
 import { RegisterComponent } from './authentication/register.component';
 
 import { ConfiguracionMedicoComponent } from './configuracion-medico/configuracion-medico.component';
 
-const appRoutes: Routes =  [
-{
-	path: '',
-	component: DashboardComponent,
-    canActivate: [AuthGuard, AdministrativoGuard]
-},
-// {
-// 	path:'turnos',
-// 	component: TurnosComponent,
-//     canActivate: [AuthGuard]
-// },
-{
-	path: 'turnos/:doctor/:idDoctor',
-	component: TurnosComponent,
-    canActivate: [AuthGuard]
-},
-{
-	path: 'pacientes',
-	component: PacientesComponent,
-    canActivate: [AuthGuard]
-},
-{
-	path: 'medico',
-	component: TurnosDelMedicoComponent,
-    canActivate: [AuthGuard, MedicoGuard]
-},
-{
-	path: 'configuracion',
-	component: ConfiguracionMedicoComponent,
-	canActivate: [AuthGuard, AdministrativoGuard]
-},
-{
-	path: 'obras',
-	component: ObrasComponent,
-	canActivate: [AuthGuard, AdministrativoGuard]
-},
-{
-	path: 'configuracion/:idDoctor',
-	component: ConfiguracionMedicoComponent,
-	canActivate: [AuthGuard]
-},
-  { path: 'login', component: LoginComponent },
-	// { path: 'registro', component: RegisterComponent }
+const appRoutes: Routes = [
+	{
+		path: '',
+		component: DashboardComponent,
+		canActivate: [AuthGuard, AdministrativoGuard]
+	},
+	// {
+	// 	path:'turnos',
+	// 	component: TurnosComponent,
+	//     canActivate: [AuthGuard]
+	// },
+	{
+		path: 'turnos/:doctor/:idDoctor',
+		component: TurnosComponent,
+	    canActivate: [AuthGuard]
+	},
+	{
+		path: 'pacientes',
+		component: PacientesComponent,
+	    canActivate: [AuthGuard]
+	},
+	{
+		path: 'medico',
+		component: TurnosDelMedicoComponent,
+	    canActivate: [AuthGuard, MedicoGuard]
+	},
+	{
+		path: 'configuracion',
+		component: ConfiguracionMedicoComponent,
+		canActivate: [AuthGuard, AdministrativoGuard]
+	},
+	{
+		path: 'obras',
+		component: ObrasComponent,
+		canActivate: [AuthGuard, AdministrativoGuard]
+	},
+	{
+		path: 'configuracion/:idDoctor',
+		component: ConfiguracionMedicoComponent,
+		canActivate: [AuthGuard]
+	},
+	{ path: 'login', component: LoginComponent },
+	{ path: 'registro', component: RegisterComponent }
 ];
 
 
-export const routing : ModuleWithProviders = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
